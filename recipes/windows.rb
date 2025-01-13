2d3d62b5b42853c20363af7e21977156db5d7d6f
Arnab-crick <royarnab994@gmail.com>
2025-01-14 01:44:54 +0530

    windows fix

--- recipes/windows.rb
+++ recipes/windows.rb
@@ -1,14 +1,16 @@
-remote_file "C:\Program Files" do
-source 'https://download.notepad-plus-plus.org/npp.8.5.6.Installer.x64.exe'
-action :create
+# Download the Notepad++ installer
+remote_file 'C:\\Temp\\npp.8.5.6.Installer.x64.exe' do
+  source 'https://download.notepad-plus-plus.org/npp.8.5.6.Installer.x64.exe'
+  action :create
 end
 
-
-windows_package "install notepad" do
-  source 'C:\Program Files\npp.8.5.6.Installer.x64.exe'
+# Install Notepad++
+windows_package 'install notepad' do
+  source 'C:\\Temp\\npp.8.5.6.Installer.x64.exe'
   installer_type :custom
-  options: '/s'
+  options '/s'
   action :install
- end
+end
+
   
   
